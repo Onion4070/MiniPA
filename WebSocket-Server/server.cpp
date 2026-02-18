19f9c2a6eceb96197c0f569dfdf10dfce0a335c4
Onion4070 <naonaosub.0704@gmail.com>
2026-02-18 16:18:24 +0900

    httpサーバのセットアップ
    localhost:9001にアクセスするとWebSocket接続が開始，messageに任意のメッセージを送信するとオウム返しされます

--- WebSocket-Server/server.cpp
+++ WebSocket-Server/server.cpp
@@ -1,53 +1,54 @@
-#include <boost/beast/core.hpp>
-#include <boost/beast/websocket.hpp>
-#include <iostream>
-#include <string>
-#include <thread>
+﻿#include <boost/beast.hpp>
+#include <boost/asio.hpp>
+#include <fstream>
 
+namespace beast = boost::beast;
+namespace http = beast::http;
+namespace websocket = beast::websocket;
 using tcp = boost::asio::ip::tcp;
 
+std::string load_file(const std::string& path) {
+    std::ifstream t(path);
+    return { std::istreambuf_iterator<char>(t),{} };
+}
+
+void session(tcp::socket socket) {
+    beast::flat_buffer buffer;
+    http::request<http::string_body> req;
+    http::read(socket, buffer, req);
+
+    // WebSocket Upgrade判定
+    if (websocket::is_upgrade(req) && req.target() == "/ws") {
+        websocket::stream<tcp::socket> ws(std::move(socket));
+        ws.accept(req);
+
+        while (1) {
+            beast::flat_buffer buf;
+            ws.read(buf);
+            ws.text(ws.got_text());
+            ws.write(buf.data());
+        }
+        return;
+    }
+
+    // HTTP処理
+    http::response<http::string_body> res{
+        http::status::ok, req.version()
+    };
+    res.set(http::field::server, "Beast");
+    res.set(http::field::content_type, "text/html");
+    res.body() = load_file("index.html");
+    res.prepare_payload();
+    http::write(socket, res);
+}
+
 int main() {
-	const auto address = boost::asio::ip::make_address("127.0.0.1");
-	const auto port = static_cast<unsigned short>(std::atoi("9001"));
+    boost::asio::io_context ioc;
+    tcp::acceptor acceptor(ioc, { tcp::v4(),9001 });
 
-	boost::asio::io_context ioc{ 1 }; // number of threads to run
-
-	tcp::acceptor acceptor{ ioc, {address, port} };
-
-	while (1) {
-		tcp::socket socket{ ioc };
-		acceptor.accept(socket);
-		std::cout << "Accepted connection from " << socket.remote_endpoint() << std::endl;
-
-		std::thread{[q = std::move(socket)]() mutable {
-			boost::beast::websocket::stream<tcp::socket> ws {std::move(q)};
-
-			// Accept the websocket handshake
-			ws.accept();
-
-			while (1) {
-				try {
-					// Receive a message from the client
-					boost::beast::flat_buffer buffer;
-					ws.read(buffer);
-
-					auto out = boost::beast::buffers_to_string(buffer.cdata());
-					std::cout << "Received: " << out << std::endl;
-
-					ws.write(buffer.data());
-				}
-				catch (boost::beast::system_error const& se) {
-					// Handle close WebSocket connection error
-					if (se.code() != boost::beast::websocket::error::closed) {
-						std::cerr << "Error: " << se.code().message() << std::endl;
-						break;
-					}
-				}
-				
-			}
-
-		}}.detach();
-	}
-
-	return 0;
+    while (1) {
+        tcp::socket socket(ioc);
+        acceptor.accept(socket);
+        std::thread(session, std::move(socket)).detach();
+    }
 }
